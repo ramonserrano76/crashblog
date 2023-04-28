a711ddbab25a130a43c6744e43d010270fe9aeb4
Ramón Serrano <ramonserrano76@gmail.com>
2023-04-28 16:25:21 -0300

    commit fixes

--- core/views.py
+++ core/views.py
@@ -1,9 +1,7 @@
-from django.http.response import HttpResponse, HttpResponseRedirect
+from django.http.response import HttpResponse
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from urllib.parse import urlparse, urlunparse
 from blog.models import Post
-from django.urls import reverse
 from django.views.generic import View
 
 class RedirectDomainView(View):
