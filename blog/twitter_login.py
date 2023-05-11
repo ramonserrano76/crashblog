9bb844ffa0b8f525eaefbac2d0d59d17b7697a0f
Ramón Serrano <ramonserrano76@gmail.com>
2023-05-11 18:08:54 -0300

    Revert "twit ouuth"
    This reverts commit fc9512ce1737def6778d80087662de76206dbae7.

--- blog/twitter_login.py
+++ blog/twitter_login.py
@@ -1,5 +1,4 @@
 import oauthlib.oauth1
-from oauthlib.oauth1 import Client
 import base64
 import hashlib
 import json
@@ -156,7 +155,8 @@ def post_tweet_with_image(request, slug, access_token):
 
     # Crear el objeto de firma OAuth1
     oauth_client = oauthlib.oauth1.Client(oauth_consumer_key, oauth_consumer_secret)
-    # Crear el params para oauth_signature
+
+
     paramssign = {
         'oauth_consumer_key': oauth_consumer_key,
         'oauth_nonce': "WQFRTfd",
